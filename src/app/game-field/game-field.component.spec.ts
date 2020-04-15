79a68990ace19573defa9a3cc3e35ccb1e850fad
Litvinov, Vlad [PS] <vlad.litvinov@cengage.com>
2020-04-15 18:23:49 +0300

    JFE-006: Add tests

--- src/app/game-field/game-field.component.spec.ts
+++ src/app/game-field/game-field.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameFieldComponent } from './game-field.component';
 
 describe('GameFieldComponent', () => {
-  let component: GameFieldComponent;
   let fixture: ComponentFixture<GameFieldComponent>;
 
   beforeEach(async(() => {
@@ -15,11 +14,109 @@ describe('GameFieldComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GameFieldComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    const component: GameFieldComponent = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should render 13 rows', () => {
+    const fieldRows: HTMLElement[] = fixture.nativeElement.querySelectorAll('.field-row');
+    expect(fieldRows.length).toBe(13);
+  });
+
+  it('should render 13 items in each row', () => {
+    const fieldRows: HTMLElement[] = fixture.nativeElement.querySelectorAll('.field-row');
+    fieldRows.forEach(row => {
+      const fieldItems = row.querySelectorAll('.field-item');
+      expect(fieldItems.length).toBe(13);
+    });
+  });
+
+  describe('initial game field', () => {
+    it('should render blank field items for corners and first/last columns/rows', () => {
+      const fieldRows: HTMLElement[] = fixture.nativeElement.querySelectorAll('.field-row');
+      const isBlankField = (item: Element) => expect(item.getAttribute('src')).toBe('../../assets/img/sea.svg');
+
+      fieldRows.forEach((row, x) => {
+        const fieldItems = row.querySelectorAll('img.field-item');
+
+        if (x === 0 || x === 12) {
+          fieldItems.forEach((item, y) => {
+            if (y !== 6) {
+              isBlankField(item);
+            }
+          });
+        }
+
+        if (x === 1 || x === 11) {
+          fieldItems.forEach((item, y) => {
+            if (y === 0 || y === 1 || y === 11 || y === 12) {
+              isBlankField(item);
+            }
+          });
+        }
+
+        if (x !== 0 && x !== 1 && x !== 11 && x !== 12 && x !== 6) {
+          fieldItems.forEach((item, y) => {
+            if (y === 0 || y === 12) {
+              isBlankField(item);
+            }
+          });
+        }
+      });
+    });
+
+    it('should render ships in default places', () => {
+      const fieldRows: HTMLElement[] = fixture.nativeElement.querySelectorAll('.field-row');
+      const isShip = (item: Element) => expect(item.getAttribute('src')).toBe('../../assets/img/ship.svg');
+
+      fieldRows.forEach((row, x) => {
+        const fieldItems = row.querySelectorAll('img.field-item');
+
+        if (x === 0 || x === 12) {
+          fieldItems.forEach((item, y) => {
+            if (y === 6) {
+              isShip(item);
+            }
+          });
+        }
+
+        if (x === 6) {
+          fieldItems.forEach((item, y) => {
+            if (y === 0 || y === 12) {
+              isShip(item);
+            }
+          });
+        }
+      });
+    });
+
+    it('should render default fields', () => {
+      const fieldRows: HTMLElement[] = fixture.nativeElement.querySelectorAll('.field-row');
+      const isDefault = (item: Element) => expect(item.getAttribute('src')).toBe('../../assets/img/default.svg');
+
+      fieldRows.forEach((row, x) => {
+        const fieldItems = row.querySelectorAll('img.field-item');
+
+        if (x === 1 || x === 11) {
+          fieldItems.forEach((item, y) => {
+            if (y !== 0 && y !== 1 && y !== 11 && y !== 12) {
+              isDefault(item);
+            }
+          });
+        }
+
+        if (x !== 0 && x !== 1 && x !== 11 && x !== 12) {
+          fieldItems.forEach((item, y) => {
+            if (y !== 0 && y !== 12) {
+              isDefault(item);
+            }
+          });
+        }
+      });
+    });
   });
 });
--- src/app/game-field/game-field.component.ts
+++ src/app/game-field/game-field.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameFieldComponent {
   public field = Array(13).fill(Array(13).fill(0));
-  public shipsCoords = [[0, 6], [6, 0], [6, 12], [12, 6]]
+  public shipsCoords = [[0, 6], [6, 0], [6, 12], [12, 6]];
 
   shouldRenderShipItem = (x, y) => {
-    return this.shipsCoords.find(item => item[0] === x && item[1] === y)
+    return this.shipsCoords.find(item => item[0] === x && item[1] === y);
   }
 
   shouldRenderBlankItem = (x, y) => {
-    return x === 0 || x === 12 || y === 0 || y === 12 || x === 1 && y === 11 || x === 1 && y === 1 || y === 1 && x === 11 || x === 11 && y === 11;
+    return x === 0
+    || x === 12
+    || y === 0
+    || y === 12
+    || x === 1
+    && y === 11 || x === 1
+    && y === 1 || y === 1
+    && x === 11 || x === 11
+    && y === 11;
   }
 }
